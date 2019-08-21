a6a4bb833beaed069d2927969704bc5f3aa31048
Filip Allberg <filip.allberg@umu.se>
2019-08-21 15:02:44 +0200

    feat: downsample a video into frames

--- main.py
+++ main.py
@@ -21,3 +21,25 @@ def divide_into_segments(filename, segment_length_in_seconds=1):
          )
 
     subprocess.call(ffmpeg_cmd.split(), stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+def downsample_video(path_to_video_file, fps=5):
+    """
+    Assumes that the given path refers to a video file and extracts an `fps` number of frames
+    from every second of the specified video.
+
+    For instance, for a video "video.mp4" `downsample_video('video.mp4', fps=5)` will yield 5 frames
+    for every second of said video. If said video is, for instance, 10 seconds long, the number of
+    frames that are produced is equal to 50.
+
+    This function does not have a return value, but has a side-effect, namely a series of image files
+    (.png) whose names are on the form `{path_to_video_file}-frame%d.png`, where the file extension
+    is removed from the `path_to_video_file` parameter.
+    """
+    import subprocess
+    from pathlib import Path
+
+    video_path = Path(path_to_video_file)
+    video_filename = video_path.stem
+    directory = video_path.parent
+
+    subprocess.call(f'ffmpeg -i {path_to_video_file} -vf fps={fps} {directory}/{video_filename}-frame%d.png'.split(), stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
