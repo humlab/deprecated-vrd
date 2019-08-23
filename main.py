eacf412ae677563501995466a780372f4c1b8f4f
Filip Allberg <filip.allberg@umu.se>
2019-08-23 10:50:19 +0200

    scout: fix multiple linting issues

--- main.py
+++ main.py
@@ -7,17 +7,19 @@ import cv2
 import subprocess
 
 """
-This code implements the fingerprinting method proposed by Zobeida Jezabel Guzman-Zavaleta
-in the thesis "An Effective and Efficient FingerprintingMethod for Video Copy Detection".
+This code implements the fingerprinting method proposed by Zobeida Jezabel
+Guzman-Zavaleta in the thesis "An Effective and Efficient Fingerprinting Method
+for Video Copy Detection".
 
-The default values used here can be assumed to stem from the same thesis, specifically
-from the section 5.4 Discussion, where the author details the parameter values that "proved"
-the "best" during her experiments.
+The default values used here can be assumed to stem from the same thesis,
+specifically from the section 5.4 Discussion, where the author details the
+parameter values that "proved" the "best" during her experiments.
 """
+
 
 def execute_ffmpeg_command(ffmpeg_cmd: str, input_video: Path, output_directory: Path) -> List[Path]:
     logger.debug(f'Removing the directory "{output_directory}" if it exists and recreating it')
-    shutil.rmtree(output_directory, ignore_errors=True) # Might fail if permissions are off
+    shutil.rmtree(output_directory, ignore_errors=True)  # Might fail if permissions are off
     output_directory.mkdir()
 
     logger.debug(f'Executing: "{ffmpeg_cmd}"')
@@ -30,28 +32,37 @@ def execute_ffmpeg_command(ffmpeg_cmd: str, input_video: Path, output_directory:
 
     return output_paths
 
+
 def divide_into_segments(input_video: Path, output_directory: Path, segment_length_in_seconds=1) -> List[Path]:
+    # -i                     input file
+    # -codec:v libx264       re-encode so we can force keyframes
+    # -force_key_frames      force keyframe every x seconds
+    # -map 0                 use the given input file to produce all outputs
+    # -f segment             output file will be multiple segments
+    # -segment_time          length of each segment expressed in seconds
     ffmpeg_cmd = (
          'ffmpeg'
-        f' -i {input_video}'                          # input file
-         ' -codec:v libx264'                          # re-encode to overwrite keyframes
+        f' -i {input_video}'
+         ' -codec:v libx264'
         f' -force_key_frames expr:gte(t,n_forced*{segment_length_in_seconds})'
-         ' -map 0'                                    # use the first input file for all outputs
-         ' -f segment'                                # output file will be multiple segments
-        f' -segment_time {segment_length_in_seconds}' # length of each segment expressed in seconds
+         ' -map 0'
+         ' -f segment'
+        f' -segment_time {segment_length_in_seconds}'
         f' {output_directory}/{input_video.stem}-segment%03d.mp4'
          )
 
     return execute_ffmpeg_command(ffmpeg_cmd, input_video, output_directory)
 
+
 def downsample_video(input_video: Path, output_directory: Path, fps=5) -> List[Path]:
     """
-    Assumes that the given path refers to a video file and extracts an `fps` number of frames
-    from every second of the specified video.
+    Assumes that the given path refers to a video file and extracts an `fps`
+    number of frames from every second of the specified video.
 
-    For instance, for a video "video.mp4" `downsample_video(Path('video.mp4'), fps=5)` will yield 5 frames
-    for every second of said video. If said video is, for instance, 10 seconds long, the number of
-    frames that are produced is equal to 50.
+    For instance, for a video "video.mp4" `downsample_video(Path('video.mp4'),
+    fps=5)` will yield 5 frames for every second of said video. If said video
+    is, for instance, 10 seconds long, the number of frames that are produced
+    is equal to 50.
 
     The return value is a list of all these frames.
     """
@@ -64,12 +75,15 @@ def downsample_video(input_video: Path, output_directory: Path, fps=5) -> List[P
 
     return execute_ffmpeg_command(ffmpeg_cmd, input_video, output_directory)
 
+
 def scale_image(image, scale_factor):
     height, width, _ = image.shape
 
-    # Another option, which is slower, would be INTER_CUBIC, which would "look" the "best"
+    # Another option for upscaling is INTER_CUBIC which is slower but
+    # produces a better looking output. Using INTER_LINEAR for now
     interpolation_method = cv2.INTER_LINEAR if scale_factor >= 1 else cv2.INTER_AREA
     return cv2.resize(image, (int(width*scale_factor), int(height*scale_factor)), interpolation=interpolation_method)
+
 
 def crop_with_central_alignment(image, m=320, n=320):
     """
@@ -82,6 +96,7 @@ def crop_with_central_alignment(image, m=320, n=320):
     img = image[starting_row:starting_row + m, starting_column:starting_column + n]
 
     return img
+
 
 def average_frames(frames):
     """
