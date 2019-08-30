1592bf1b9e7889302ec0c988c4fa5340f890d0c4
Filip Allberg <filip.allberg@gmail.com>
2019-08-30 09:23:14 +0200

    docs: document caveat regarding "average"

--- image_transformation.py
+++ image_transformation.py
@@ -1,12 +1,17 @@
 import numpy as np
 import cv2
 
+from typing import List
 
-def average(images):
-    # Assume all images are of the same dimensions
-    height, width, channels = images[0].shape
 
-    avg = np.zeros((height, width, channels), np.float)
+def average(images: List[np.ndarray]) -> np.ndarray:
+    """Average all the elements in the input matrices producing a new matrix
+    such that the output is a new image, and thus the new "average" is not
+    a mathematical average, but rounded to the nearest integer values.
+
+    All input arrays are assumed to be of the same size.
+    """
+    avg = np.zeros(images[0].shape, np.float)
 
     for image in images:
         avg += image/len(images)
