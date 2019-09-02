fb072f63be3f2c6170922be5114af6bb511a82c6
Filip Allberg <filip.allberg@gmail.com>
2019-09-02 14:37:40 +0200

    feat: find smeared mean across all channels

--- notebooks/2.0-block-processing.py
+++ notebooks/2.0-block-processing.py
@@ -38,7 +38,7 @@ plt.imshow(simple_checkerboard)
 
 
 # %% [markdown]
-# And then, if we combine several of these
+# And then, if we combine several of these boards into a new checkerboard
 
 # %%
 def checkerboard(rows, columns, channels, color):
@@ -64,7 +64,39 @@ top_half    = np.hstack((black_board, red_board))
 bottom_half = np.hstack((green_board, blue_board))
 
 board = np.vstack((top_half, bottom_half))
-
 plt.imshow(board)
 
+# %% [markdown]
+# we can then operate on blocks that are the same size as each "sub"-board. Here, we first view the board as a matrix of blocks,
+
 # %%
+import skimage.util
+
+assert(black_board.shape == red_board.shape == green_board.shape == blue_board.shape)
+block_shape = black_board.shape
+
+# view the board as a matrix of blocks (of shape block_shape)
+view = skimage.util.view_as_blocks(board, block_shape)
+
+# collapse the last two dimensions in one
+flatten_view = view.reshape(view.shape[0], view.shape[1], -1)
+
+# %% [markdown]
+# And then, we compute the mean of each checkerboard,
+
+# %%
+# Compute the mean for each sub-checkerboard,
+mean_view = np.mean(flatten_view, axis=2)
+
+# Then, the number of means should be equal to the number of boards,
+boards = [black_board.shape, red_board.shape, green_board.shape, blue_board.shape]
+assert(len(boards) == len(mean_view.flatten()))
+
+# %% [markdown]
+# And now, if we plot that,
+
+# %%
+plt.imshow(mean_view)
+
+# %% [markdown]
+# We observe that the mean of the three colored checkerboards are the same and this is expected as the mean is smeared across all channels here.
