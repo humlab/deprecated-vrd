5f85c7e2ac78fbdd3636e08e0ffaf60a87ecdbc5
Filip Allberg <filip.allberg@umu.se>
2019-09-05 14:53:04 +0200

    refact: separation of concerns

--- video_reuse_detector/main.py
+++ video_reuse_detector/main.py
@@ -1,8 +1,10 @@
 import cv2
-import video
+import numpy as np
 
+from typing import List, Tuple
 from pathlib import Path
 
+import video
 import fingerprint
 
 """
@@ -25,26 +27,61 @@ def imwrite(filename: Path, image):
     cv2.imwrite(str(filename), image)
 
 
+def extract_frames(segment_id: int,
+                   segment: Path,
+                   output_directory: Path) -> Tuple[List[np.ndarray],
+                                                    List[Path]]:
+    frames_dir = output_directory / 'frames'
+    frames_output_directory = frames_dir / f'segment{segment_id:03}'
+    frame_paths = video.downsample(segment, frames_output_directory)
+    frames = [imread(filename) for filename in frame_paths]
+
+    return (frames, frame_paths)
+
+
+def extract_fingerprints_from_segment(segment_id: int,
+                                      segment: Path,
+                                      output_directory: Path):
+    frames, _ = extract_frames(segment_id, segment, output_directory)
+    kf = fingerprint.Keyframe.from_frames(input_video, segment_id, frames)
+    th = fingerprint.Thumbnail.from_keyframe(kf)
+    cc = fingerprint.ColorCorrelation.from_keyframe(kf)
+
+    return (kf, th, cc)
+
+
+def store_keyframe(kf: fingerprint.Keyframe, output_directory: Path):
+    kf_dir = output_directory / 'keyframes'
+    input_video = kf.metadata.video_source
+    segment_id = kf.metadata.segment_id
+
+    dst = kf_dir / f'{input_video.stem}-keyframe{segment_id:03}.png'
+
+    imwrite(dst, kf.image)
+
+
+def store_thumbnail(th: fingerprint.Thumbnail, output_directory: Path):
+    thumbs_dir = output_directory / 'thumbs'
+    input_video = th.metadata.video_source
+    segment_id = th.metadata.segment_id
+
+    dst = thumbs_dir / f'{input_video.stem}-thumb{segment_id:03}.png'
+
+    imwrite(dst, th.image)
+
+
 def produce_fingerprints(input_video: Path, output_directory: Path):
     # TODO: Produce audio fingerprints, this just creates keyframes
     # TODO: Clean-up intermediary directories
-    segments_dir = output_directory / 'segments'
-    frames_dir = output_directory / 'frames'
-    segments = video.segment(input_video, segments_dir)
+    segments = video.segment(input_video, output_directory / 'segments')
 
     for segment_id, segment in segments.items():
-        frames_output_directory = frames_dir / f'segment{segment_id:03}'
-        frame_paths = video.downsample(segment, frames_output_directory)
-        frames = [imread(filename) for filename in frame_paths]
+        kf, th, cc = extract_fingerprints_from_segment(segment_id,
+                                                       segment,
+                                                       output_directory)
 
-        kf_dir = output_directory / 'keyframes'
-        kf = fingerprint.Keyframe.from_frames(input_video, segment_id, frames)
-
-        imwrite(kf_dir / f'{input_video.stem}-keyframe{segment_id:03}.png', kf.image)  # noqa: E501
-
-        thumbs_dir = output_directory / 'thumbs'
-        thumb = fingerprint.Thumbnail.from_keyframe(kf)
-        imwrite(thumbs_dir / f'{input_video.stem}-thumb{segment_id:03}.png', thumb.image)  # noqa: E501
+        store_keyframe(kf, output_directory)
+        store_thumbnail(th, output_directory)
 
 
 if __name__ == "__main__":
