129e63cc2de2c8c244e349ab80269d06b33e3b12
Filip Allberg <filip.allberg@umu.se>
2019-09-12 09:23:50 +0200

    feat: compute normalized crossed correlation

--- video_reuse_detector/main.py
+++ video_reuse_detector/main.py
@@ -1,9 +1,16 @@
+import numpy as np
+
 from typing import List
 from pathlib import Path
 from loguru import logger
 
 from video_reuse_detector.fingerprint import Keyframe, \
-    FingerprintCollection, produce_fingerprints
+    FingerprintCollection, Thumbnail
+
+
+def load_keyframe(file_path: Path) -> Keyframe:
+    logger.debug(f'Loading keyframe from file={file_path}')
+    return Keyframe.from_file(file_path)
 
 
 def load_keyframes(input_directory: str) -> List[Keyframe]:
@@ -12,8 +19,7 @@ def load_keyframes(input_directory: str) -> List[Keyframe]:
     keyframes = []
 
     for f in glob(f'{input_directory}/*.png'):
-        logger.debug(f'Loading keyframe from file={f}')
-        keyframes.append(Keyframe.from_file(Path(f)))
+        keyframes.append(load_keyframe(Path(f)))
 
     return keyframes
 
@@ -24,6 +30,21 @@ def load_fingerprints(input_directory: str) -> List[FingerprintCollection]:
     return list(map(FingerprintCollection, keyframes))
 
 
+def normalized_crossed_correlation(qFp: np.ndarray, rFp: np.ndarray) -> float:
+    left = qFp - np.mean(qFp)
+    right = rFp - np.mean(rFp)
+    dividend = np.sum(left*right)
+    divisor = np.sqrt(np.sum(left**2) * np.sum(right**2))
+
+    correlation = dividend / divisor
+
+    return correlation
+
+
+def compare_thumbnails(query: Thumbnail, reference: Thumbnail) -> float:
+    return normalized_crossed_correlation(query.image, reference.image)
+
+
 if __name__ == "__main__":
     import argparse
 
@@ -31,14 +52,20 @@ if __name__ == "__main__":
         description='Video reuse search tool')
 
     parser.add_argument(
-        'input_video',
-        help='The video to compare against the reference set')
+        'input_keyframes',
+        help='A directory containing keyframes for our query video')
 
     parser.add_argument(
-        'reference_set',
-        help='A directory containing videos to query against')
+        'reference_keyframes',
+        help='A directory containing keyframes for our reference video')
 
     args = parser.parse_args()
 
-    query_prints = produce_fingerprints(Path(args.input_video), Path('foo'))
-    reference_fingerprints = load_keyframes(args.reference_set)
+    query_prints = load_fingerprints(args.input_keyframes)
+    reference_fingerprints = load_fingerprints(args.reference_keyframes)
+
+    logger.debug('Comparing the first pair of thumbnails')
+    query = query_prints[0].th
+    reference = reference_fingerprints[0].th
+
+    print(compare_thumbnails(query, reference))
