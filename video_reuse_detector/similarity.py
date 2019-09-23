9bd666ded26691010cbc87e68e9d5b70cebb1b1c
Filip Allberg <filip.allberg@gmail.com>
2019-09-23 16:35:28 +0200

    perf: faster orb-descriptor comparison

--- video_reuse_detector/similarity.py
+++ video_reuse_detector/similarity.py
@@ -1,18 +1,8 @@
 import numpy as np
-from typing import List
-
-
-def bitfield(n: int) -> List[int]:
-    return [1 if digit == '1' else 0 for digit in np.binary_repr(n)]
 
 
 def hamming_distance(n1: int, n2: int) -> float:
-    def H(v: List[int], u: List[int]) -> float:
-        from scipy.spatial.distance import hamming
-
-        return hamming(u, v)
-
-    return H(bitfield(n1), bitfield(n2))
+    return bin(n1 ^ n2).count('1')/32.0
 
 
 def normalized_crossed_correlation(qFp: np.ndarray, rFp: np.ndarray) -> float:
