b2bb16d4a049aadb7fa11aa060009aa8fad7d78e
Filip Allberg <filip.allberg@gmail.com>
2019-09-25 15:59:50 +0200

    refact: remove perf comparison

--- tests/test_orb.py
+++ tests/test_orb.py
@@ -4,8 +4,6 @@ import skimage
 from skimage import data
 from skimage import transform as tf
 
-from timeit import default_timer as timer
-
 from video_reuse_detector.orb import ORB
 
 
@@ -17,10 +15,7 @@ class TestOrb(unittest.TestCase):
         orb = ORB.from_image(astronaut)
 
         # The ORB features of an image are 100% similar to itself
-        naive = orb.similar_to_naive(orb)
-        lu = orb.similar_to_lu(orb)
-
-        self.assertEqual(naive, lu)
+        self.assertEqual(orb.similar_to_lu(orb), orb.similar_to_lu(orb))
 
     def test_orb_similarity_rotation(self):
         img1 = skimage.img_as_ubyte(data.astronaut()).astype('uint8') * 255
@@ -33,26 +28,6 @@ class TestOrb(unittest.TestCase):
         orb2 = ORB.from_image(img2)
         orb3 = ORB.from_image(img3)
 
-        start = timer()
-        naive12 = orb1.similar_to_naive(orb2)
-        end = timer()
-        print(f'naive12 time: {end - start}')
-
-        start = timer()
-        naive13 = orb1.similar_to_naive(orb3)
-        end = timer()
-        print(f'naive13 time: {end - start}')
-        self.assertEqual(naive12[1], naive13[1])
-
-        start = timer()
         lu12 = orb1.similar_to_lu(orb2)
-        end = timer()
-
-        print(f'lu12 time: {end - start}')
-        start = timer()
         lu13 = orb1.similar_to_lu(orb3)
-        end = timer()
-        print(f'lu13 time: {end - start}')
         self.assertEqual(lu12[1], lu13[1])
-
-        self.assertEqual(naive12[1], lu12[1])
