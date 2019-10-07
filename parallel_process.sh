244e4657b179799dbfeeb20ec5bf2f2face6d6d6
Filip Allberg <filip.allberg@gmail.com>
2019-10-07 16:12:23 +0200

    feat: accept input as a directory

--- parallel_process.sh
+++ parallel_process.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 start_time=`date +%s`
-parallel make process INPUT_FILE={} ::: $1
+parallel make process INPUT_FILE={} ::: $(ls -d $1/*)
 end_time=`date +%s`
 
 echo "Execution time was $(expr $end_time - $start_time) seconds."
