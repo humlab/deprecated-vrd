0b0a61877fbfe19697c324f82a94f6cf40796bb3
Filip Allberg <filip.allberg@gmail.com>
2019-10-18 13:36:14 +0200

    fix: fix linting errors

--- app/server.py
+++ app/server.py
@@ -1,19 +1,22 @@
-from flask import Flask, flash, request, redirect, url_for, session
+from flask import Flask, request
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
-        return 'Hello, world!'
+    return 'Hello, world!'
 
-@app.route('/upload', methods=['GET','POST'])
+
+@app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         f = request.files['file']
         f.save(secure_filename(f.filename))
 
         return 'file uploaded successfully!'
+
 
 if __name__ == '__main__':
     app.run()
