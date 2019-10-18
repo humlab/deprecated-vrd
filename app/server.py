1fd72113a9d8611792488a24efb75046f737fc1c
Filip Allberg <filip.allberg@gmail.com>
2019-10-18 15:03:08 +0200

    feat: fix CORS warning

--- app/server.py
+++ app/server.py
@@ -1,8 +1,9 @@
 from flask import Flask, request
 from werkzeug.utils import secure_filename
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)
 
 @app.route('/')
 def hello_world():
