516dc62009582f2681cecb3feae9c26ebbd25fbb
Filip Allberg <filip.allberg@gmail.com>
2019-10-21 14:32:24 +0200

    feat: list uploaded files

--- app/frontend/video_reuse_detector/src/App.js
+++ app/frontend/video_reuse_detector/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Dropzone from "react-dropzone-uploader";
+import axios from "axios";
 
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.min.css";
@@ -7,33 +8,90 @@ import "react-toastify/dist/ReactToastify.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "react-dropzone-uploader/dist/styles.css";
 
-const App = () => {
-  const getUploadParams = () => {
+const FileTable = props => {
+  const { files } = props;
+  return (
+    <div className="files">
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">Name</th>
+            <th scope="col">State</th>
+          </tr>
+        </thead>
+        <tbody>
+          {files.map(f => (
+            <FileListing key={f.filename} file={f} />
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+const FileListing = props => {
+  const { filename, state } = props.file;
+
+  return (
+    <tr>
+      <th scope="row">{filename}</th>
+      <td>{state}</td>
+    </tr>
+  );
+};
+
+class App extends React.Component {
+  state = {
+    files: []
+  };
+
+  componentDidMount() {
+    this.listFiles();
+  }
+
+  listFiles = async () => {
+    axios.get("http://localhost:5000/list").then(res => {
+      this.setState({ files: res.data });
+    });
+  };
+
+  getUploadParams = () => {
     return { url: "http://localhost:5000/upload" };
   };
 
-  const handleChangeStatus = ({ meta, remove }, status) => {
+  handleChangeStatus = ({ meta, remove }, status) => {
     if (status === "headers_received") {
       toast.success(`${meta.name} uploaded!`);
+      this.listFiles();
       remove();
     } else if (status === "aborted") {
       toast.error(`${meta.name}, upload failed...`);
     }
   };
 
-  return (
-    <div className="app text-center">
-      <Dropzone
-        getUploadParams={getUploadParams}
-        onChangeStatus={handleChangeStatus}
-        styles={{
-          dropzone: { width: 400, height: 200 },
-          dropzoneActive: { borderColor: "green" }
-        }}
-      />
-      <ToastContainer />
-    </div>
-  );
-};
+  render() {
+    return (
+      <div className="app container">
+        <div className="row">
+          <div className="col">
+            <Dropzone
+              getUploadParams={this.getUploadParams}
+              onChangeStatus={this.handleChangeStatus}
+              styles={{
+                dropzoneActive: { borderColor: "green" }
+              }}
+            />
+            <ToastContainer />
+          </div>
+        </div>
+        <div className="row mt-5">
+          <div className="col">
+            <FileTable files={this.state.files} />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
--- app/server.py
+++ app/server.py
@@ -1,9 +1,28 @@
 from flask import Flask, request
 from werkzeug.utils import secure_filename
 from flask_cors import CORS
+from pathlib import Path
+import json
+import os
+from typing import Dict
+
+
+def unprocessed_file(path: Path) -> Dict[str, str]:
+    return {'filename': str(path.name), 'state': 'UNPROCESSED'}
+
 
 app = Flask(__name__)
 CORS(app)
+
+BASE_DIR = Path(os.path.dirname(os.path.dirname(__file__)))
+UPLOAD_DIRECTORY = BASE_DIR / 'raw'
+
+if not UPLOAD_DIRECTORY.exists():
+    UPLOAD_DIRECTORY.mkdir()
+
+initial_file_names = list(UPLOAD_DIRECTORY.glob('**/*'))
+unprocessed_files = list(map(unprocessed_file, initial_file_names))
+files = unprocessed_files
 
 
 @app.route('/')
@@ -11,13 +30,20 @@ def hello_world():
     return 'Hello, world!'
 
 
-@app.route('/upload', methods=['GET', 'POST'])
+@app.route('/upload', methods=['POST'])
 def upload_file():
     if request.method == 'POST':
         f = request.files['file']
-        f.save(secure_filename(f.filename))
+        filename = secure_filename(f.filename)
+        f.save(str(UPLOAD_DIRECTORY / filename))
+        files.append({'filename': filename, 'state': 'UNPROCESSED'})
 
-        return 'file uploaded successfully!'
+        return '{filename} uploaded successfully!'
+
+
+@app.route('/list')
+def list_files():
+    return json.dumps(files)
 
 
 if __name__ == '__main__':
