881e3b608acf129dc0d8d2742ae2eb9790e0dbb2
Filip Allberg <filip.allberg@gmail.com>
2019-10-29 07:41:09 +0100

    refact: modularize backend

--- app/manage.py
+++ app/manage.py
@@ -1,7 +1,8 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
-from app.server import app, db
+from app.server import app
+from app.models.base import db
 
 migrate = Migrate(app, db)
 manager = Manager(app)
