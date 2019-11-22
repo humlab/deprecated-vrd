82aaeac247b53cdb5cee7e02d5b7a7554e3da7c6
Filip Allberg <filip.allberg@gmail.com>
2019-11-22 16:06:17 +0100

    refact: remove unused model

--- middleware/models/processed_files.py
+++
@@ -1,9 +0,0 @@
-from . import db
-
-
-class ProcessedFileModel(db.Model):  # type: ignore
-    __tablename__ = 'processed_files'
-
-    pk = db.Column(db.Integer(), primary_key=True)
-
-    video_name = db.Column(db.String())
