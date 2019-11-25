2340cbc3028298c8d45dc4aaf1bac72b81e097c1
Kajsa Palm <kapa0037@ad.umu.se>
2019-11-25 13:51:10 +0100

    fix lint

--- frontend/src/Components/Files/FileListing.js
+++ frontend/src/Components/Files/FileListing.js
@@ -2,17 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const FileListing = ({ filename, state }) => (
-    <tr>
-      <th scope="row">{filename}</th>
-      <td>{state}</td>
-    </tr>
-  );
-  
-  FileListing.propTypes = {
-    filename: PropTypes.string.isRequired,
-    state: PropTypes.string.isRequired
-  };
+  <tr>
+    <th scope="row">{filename}</th>
+    <td>{state}</td>
+  </tr>
+);
 
-  
-  export default FileListing;
-  
+FileListing.propTypes = {
+  filename: PropTypes.string.isRequired,
+  state: PropTypes.string.isRequired
+};
+
+
+export default FileListing;
