humlab/deprecated-vrd

commit 41c15edae0a89508e299d04b204b2b02240fb737

refact: expose ORB-keypoints

video_reuse_detector/orb.py
@@ -40,6 +40,7 @@ def detect_and_extract(image: np.ndarray):
 
 @dataclass
 class ORB:
+    keypoints: List[List[int]]
     descriptors: List[List[int]]
 
     @staticmethod
@@ -51,12 +52,12 @@ class ORB:
         # grayscale(folded, 16).astype(np.uint8)
         f_grayscale = image_transformation.grayscale(folded)
 
-        _, des = detect_and_extract(f_grayscale)
+        kps, des = detect_and_extract(f_grayscale)
 
         if des is None:
             des = []  # No features found
 
-        return ORB(des)
+        return ORB(kps, des)
 
     @staticmethod
     def compute_percentage(no_of_good_matches, no_of_possible_matches):

commit 04d40959633fce5cf5983426c6b3e0ae712030eb

fix: fix model conversion error
Caused by 41c15ed

middleware/models/fingerprint_collection.py
@@ -81,7 +81,7 @@ class FingerprintCollectionModel(db.Model):  # type: ignore
 
         # TODO: Thumbnails aren't guaranteed to be this size
         # right now. Expose class constant for defaults?
-        thumbnail = np.resize(thumbnail, (30, 30, 3))
+        thumbnail = np.resize(thumbnail, (30, 30))
 
         cc = ColorCorrelation.from_number(self.color_correlation)
 
@@ -98,6 +98,7 @@ class FingerprintCollectionModel(db.Model):  # type: ignore
         np_thumb = fpc.thumbnail.image
 
         assert np_thumb.dtype == np.float64  # important!
+        assert np_thumb.shape == (30, 30)
 
         encoded = base64.b64encode(np_thumb)
         color_correlation = fpc.color_correlation.as_number

video_reuse_detector/orb.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import List, TypeVar
 
 import cv2
@@ -18,7 +18,7 @@ def flatten(nested_list: List[List[T]]) -> List[T]:
 
 # Look-up table for similarity comparison.
 # See https://stackoverflow.com/a/58098034/5045375
-lu = sum(np.unravel_index(np.arange(256), 8 * (2, )))
+lu = sum(map(np.uint8, np.unravel_index(np.arange(256), 8*(2,))))
 
 
 def detect_and_extract(image: np.ndarray):
@@ -41,8 +41,8 @@ def detect_and_extract(image: np.ndarray):
 
 @dataclass
 class ORB:
-    keypoints: List[List[int]]
     descriptors: List[List[int]]
+    keypoints: List[List[int]] = field(default_factory=list)
 
     @staticmethod
     def from_image(image: np.ndarray) -> 'ORB':
@@ -52,13 +52,12 @@ class ORB:
         # grayscale = image_transformation.normalized_grayscale
         # grayscale(folded, 16).astype(np.uint8)
         f_grayscale = image_transformation.grayscale(folded)
-
         kps, des = detect_and_extract(f_grayscale)
 
         if des is None:
             des = []  # No features found
 
-        return ORB(kps, des)
+        return ORB(des, kps)
 
     @staticmethod
     def compute_percentage(no_of_good_matches, no_of_possible_matches):
@@ -88,6 +87,6 @@ class ORB:
             lu[(a[:, None, None] ^ b[None, :, None])  # type: ignore
                .view(np.uint8)].sum(2) <= 32 - int(32*th))
 
-        all_possible_matches = len(a) * len(b)
+        all_possible_matches = a.shape[0] * b.shape[0]
 
         return ORB.compute_percentage(good_matches, all_possible_matches)[1]

middleware/tests/unit/test_fingerprint_collection_model.py
@@ -1,0 +1,41 @@
+import unittest
+from pathlib import Path
+
+import numpy as np
+
+from middleware.models.fingerprint_collection import FingerprintCollectionModel
+from video_reuse_detector.fingerprint import extract_fingerprint_collection
+
+
+class FingerprintCollectionModelTest(unittest.TestCase):
+    def test_model_conversion(self):
+        # TODO: Make it possible to fingerprint a partial video, this is to
+        # reduce the number of files we have in our test-directory. I.e., it
+        # would be much nicer to have the entire thing stored there and
+        # just "slice" out a suitable duration of the video.
+        video_path = Path(
+            Path.cwd() / 'static/tests/videos/panorama_augusti_1944_000030_000040_10s.mp4'
+        )  # noqa: E501
+        assert video_path.exists()
+
+        root_output_directory = Path.cwd() / "interim"
+
+        fps = extract_fingerprint_collection(
+            video_path, root_output_directory
+        )
+
+        # Convert the fingerprint into our database representation and back
+        # again, compare for equality
+        fpc = fps[0]
+        assert(fpc.orb is not None)
+
+        model = FingerprintCollectionModel.from_fingerprint_collection(fpc)
+        restored = model.to_fingerprint_collection()
+
+        self.assertTrue(np.array_equal(fpc.thumbnail.image, restored.thumbnail.image))
+
+        cc_similarity = fpc.color_correlation.similar_to(restored.color_correlation)
+        self.assertTrue(cc_similarity == 1.0)
+        self.assertEqual(fpc.video_name, restored.video_name)
+        self.assertEqual(fpc.segment_id, restored.segment_id)
+        self.assertTrue(fpc.orb.similar_to(restored.orb) > 0.99)