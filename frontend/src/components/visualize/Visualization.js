3a7bfc252cbc260ea3bd9b99b70b744d1877eead
Filip Allberg <filip.allberg@gmail.com>
2020-02-27 13:01:34 +0100

    feat: render to canvas

--- frontend/src/components/visualize/Visualization.js
+++ frontend/src/components/visualize/Visualization.js
@@ -21,6 +21,37 @@ class Canvas extends React.Component {
 
   canvas = null;
   ctx = null;
+  shouldRender = false;
+
+  componentDidMount() {
+    this.ctx = this.canvas.getContext('2d');
+    this.ctx.strokeStyle = this.props.strokeStyle;
+    this.ctx.lineWidth = this.props.lineWidth;
+
+    // Perform initial render
+    console.log('Component did mount: prompting initial render');
+    this.shouldRender = true;
+    requestAnimationFrame(this.updateCanvas);
+  }
+
+  updateCanvas = () => {
+    if (!this.shouldRender) {
+      return;
+    }
+
+    console.log('Clearing canvas');
+    this.ctx.clearRect(0, 0, this.props.width, this.props.height);
+
+    console.log('Rendering items to canvas');
+    const rect = {
+      x: 50,
+      y: 50,
+      w: 50,
+      h: 50
+    };
+
+    this.ctx.strokeRect(rect.x, rect.y, rect.w, rect.h);
+  };
 
   render() {
     return (
@@ -38,5 +69,6 @@ class Canvas extends React.Component {
 Canvas.propTypes = {
   lineWidth: PropTypes.number.isRequired,
   width: PropTypes.number.isRequired,
-  height: PropTypes.number.isRequired
+  height: PropTypes.number.isRequired,
+  strokeStyle: PropTypes.string.isRequired
 };
